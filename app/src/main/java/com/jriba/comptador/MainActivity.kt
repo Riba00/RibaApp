3a6297592c3f4a39c6e6ea3b68772b985e56148b
Riba00 <jordiriba@iesebre.com>
2022-10-05 17:09:21 +0200

    App comptador

--- app/src/main/java/com/jriba/comptador/MainActivity.kt
+++ app/src/main/java/com/jriba/comptador/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.jriba.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/jriba/comptador/ExampleUnitTest.kt
+++ app/src/test/java/com/jriba/comptador/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.jriba.comptador
 
 import org.junit.Test
 
