9c2558e8ed5f78bf5c1087989cdb72377ba63e86
Riba00 <jordiriba@iesebre.com>
2022-10-19 16:27:34 +0200

    CoptadorAndroidPrimersPassos Acabat

--- app/src/main/java/com/jriba/ribacomptador/MainActivity.kt
+++ app/src/main/java/com/jriba/ribacomptador/MainActivity.kt
@@ -2,10 +2,42 @@ package com.jriba.ribacomptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton : Button
+    internal lateinit var timeTextView : TextView
+    internal lateinit var counterTextView : TextView
+    internal var counter = 0
+    internal var time = 60
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+        tapMeButton.setOnClickListener{
+            incrementCounter()
+            // TODO -> Iniciar el comptador
+        }
+
+//        timeTextView.text = time.toString()
+        timeTextView.text = getString(R.string.timeText, time)
+
+
+
+
+    }
+
+    private fun incrementCounter(){
+
+        counter += 1
+        counterTextView.text = counter.toString()
+
     }
 }
