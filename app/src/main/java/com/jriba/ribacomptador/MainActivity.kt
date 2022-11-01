fd13316d61c5734b30f71e4217d57ba9571bfe7f
Riba00 <jordiriba@iesebre.com>
2022-11-01 16:16:54 +0100

    Video debbuging acabat

--- app/src/main/java/com/jriba/ribacomptador/MainActivity.kt
+++ app/src/main/java/com/jriba/ribacomptador/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.jriba.ribacomptador
 
-import android.content.IntentSender.OnFinished
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val INITIAL_TIME = 3
+    private val TAG = MainActivity::class.java.simpleName
+
     internal lateinit var tapMeButton: Button
     internal lateinit var timeTextView: TextView
     internal lateinit var counterTextView: TextView
 
     internal var counter = 0
-    internal var time = 60
+    internal var time = INITIAL_TIME
 
     internal var appStarted = false
     internal lateinit var countDownTimer: CountDownTimer
-    internal val initialCountDownTimer: Long = 6000
+    internal val initialCountDownTimer: Long = time.toLong() * 1000
     internal val internalCountDownTimer: Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"Hola mon")
+
+        Log.d(TAG, counter.toString())
+        Log.d(TAG, time.toString())
 
         initCountdown()
 
@@ -35,15 +44,10 @@ class MainActivity : AppCompatActivity() {
         tapMeButton.setOnClickListener {
             if (!appStarted) {
                 startGame()
-
             }
-
             incrementCounter()
         }
-
         timeTextView.text = getString(R.string.timeText, time)
-
-
     }
 
     private fun startGame() {
@@ -72,13 +76,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun endGame(){
-        Toast.makeText(this, getString(R.string.endGame),Toast.LENGTH_LONG).show()
-
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.endGame, counter), Toast.LENGTH_LONG).show()
+        resetGame()
     }
 
-    private fun resetGame(){
+    private fun resetGame() {
+        counter = 0
+        counterTextView.text = counter.toString()
 
+        time = INITIAL_TIME
+        timeTextView.text = time.toString()
+        initCountdown()
+
+        appStarted = false
 
     }
 }
